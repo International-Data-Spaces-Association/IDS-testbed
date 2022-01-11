03a7e91cd077b9774e60350f431cefbbc44e3ffc
aitorce <82808687+aitorce@users.noreply.github.com>
2022-01-11 04:43:53 -0500

    Delete Validation directory
    No longer needed.

--- Testbed/Validation/DSC/Karate API tests/src/test/java/basic-auth.js
+++
@@ -1,6 +0,0 @@
-function fn(creds) {
-    var temp = creds.username + ':' + creds.password;
-    var Base64 = Java.type('java.util.Base64');
-    var encoded = Base64.getEncoder().encodeToString(temp.toString().getBytes());
-    return 'Basic ' + encoded;
-  }
